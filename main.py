485f3bc5318f9cd8c94787b943a3d434502b7784
Jinsong Huang <huangjs@pku.edu.cn>
2020-07-21 16:37:30 +0800

    add argparser

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
+import argparse
 from approch import Model
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('--niter', type=int, default=20, help='number of epochs per task')
+    parser.add_argument('--lr', type=float, default=1e-4, help='learning rate')
+    parser.add_argument('--beta', type=float, default=1., help='hyperparameter beta')
+    parser.add_argument('--gamma', type=float, default=1., help='hyperparameter gamma')
+    
+    opt = parser.parse_args()
+    
     model = Model()
-    model.train(20, 1e-4, 1, 1)
+    model.train(opt.niter, opt.lr, opt.beta, opt.gamma)
 
 
 if __name__ == '__main__':
